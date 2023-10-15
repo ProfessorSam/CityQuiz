1b89e42a8838671717a08d3e9a1a42c6eca1fb85
ProfessorSam <76458774+ProfessorSam@users.noreply.github.com>
2023-10-15 20:49:15 +0200

    Secure time settings

--- src/main/java/com/github/professorSam/handler/AdminSetTimePostHandler.java
+++ src/main/java/com/github/professorSam/handler/AdminSetTimePostHandler.java
@@ -1,8 +1,10 @@
 package com.github.professorSam.handler;
 
+import com.github.professorSam.Main;
 import com.github.professorSam.db.Database;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import io.javalin.http.HttpStatus;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,13 @@ public class AdminSetTimePostHandler implements Handler {
     private static final Logger logger = LoggerFactory.getLogger("EndTimePost");
     @Override
     public void handle(@NotNull Context context) throws Exception {
+        if(Main.getInstance().getAdminToken() != null){
+            String token = context.queryParam("token");
+            if(token == null || !token.equals(Main.getInstance().getAdminToken())){
+                context.status(HttpStatus.UNAUTHORIZED);
+                return;
+            }
+        }
         String time = context.formParam("endtime");
         if(time == null){
             context.redirect("/admin");
